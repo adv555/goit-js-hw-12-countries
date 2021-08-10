b8339b4d37ae3a6f3a443307ebb9f4a6b17b510c
Olena Boldysheva <76661298+adv555@users.noreply.github.com>
2021-08-10 20:29:23 +0300

    add pnotify

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
+// import { template } from 'handlebars';
 import fetchCountries from './js/fetchCountries';
 import countryCardTpl from './templates/country-card.hbs';
 import countryListTpl from './templates/country-list.hbs';
+import '@pnotify/core/dist/BrightTheme.css';
+import { error } from '@pnotify/core';
+
 console.log(fetchCountries);
 var debounce = require('lodash.debounce');
 // const Handlebars = require('handlebars');
 
 // ======= refs
 const refs = {
-  input: document.querySelector('.input-box'),
+  input: document.querySelector('#searchQuery'),
   cards: document.querySelector('.js-list'),
 };
 
@@ -19,16 +23,53 @@ function onSearch(e) {
   e.preventDefault();
   const query = e.target.value;
   // console.log(query);
-  fetchCountries(query).then(data => creatMarckup(data, countryCardTpl));
+  fetchCountries(query)
+    .then(data => {
+      if (data.length === 1) {
+        return creatMarckup(data, countryCardTpl);
+      } else if (data.length > 2 && data.length < 10) {
+        return creatMarckup(data, countryListTpl);
+      } else if (data.length > 10) {
+        console.log(data.length);
+        return createNotice();
+      } else if (data.status === 404) {
+        console.log(data.status);
+        return createNotice404();
+      }
+    })
+    .catch(error => createNotice(error));
+  // fetchCountries(query).then(data => creatMarckup(data, countryCardTpl));
 }
 
 // ========= marckup
-// function creatMarckup(countries, template)) {
-//   refs.cards.insertAdjacentHTML('afterbegin', articlesTpl(articles));
+function creatMarckup(data, template) {
+  refs.cards.innerHTML = template(data);
+  console.log((refs.cards.innerHTML = template(data)));
+}
+// ========= marckup 2
+// function creatMarckup(countries, template) {
+//   const markup = countries.map(country => template(country)).join('');
+//   // console.log(markup);
+//   refs.cards.insertAdjacentHTML('afterbegin', markup);
 // }
-// ========= marckup
-function creatMarckup(countries, template) {
-  const markup = countries.map(country => template(country)).join('');
-  // console.log(markup);
-  refs.cards.insertAdjacentHTML('afterbegin', markup);
+
+// =========
+function createNotice() {
+  refs.cards.innerHTML = '';
+  return error({
+    type: 'notice',
+    hide: true,
+    delay: 500,
+    text: 'Too many matches found. Please enter a more specific query!',
+  });
+}
+
+function createNotice404() {
+  refs.cards.innerHTML = '';
+  return error({
+    type: 'notice',
+    hide: true,
+    delay: 500,
+    text: 'No country has been found. Please enter a more specific query!',
+  });
 }
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,25 +1,6 @@
-// export default class CountriesApiService {
-//   constructor() {
-//     this.searchQuery = '';
-//   }
-
-//   fetchCountries() {
-//     console.log(this);
-//     const url = `https://restcountries.eu/rest/v2/name/${this.searchQuery}`;
-//     return fetch(url)
-//       .then(resolve => resolve.json())
-//       .then(data => console.log(data));
-//   }
-//   get query() {
-//     return this.searchQuery;
-//   }
-//   set query(newQuery) {
-//     this.searchQuery = newQuery;
-//   }
-// }
-// ======== fn
+// ======== fetch Api fn
 export default function fetchCountries(searchQuery) {
   const url = `https://restcountries.eu/rest/v2/name/${searchQuery}`;
 
-  return fetch(url).then(resolve => resolve.json());
+  return fetch(url).then(response => response.json());
 }
